5c74156052199b550e5ccd07ab3f20005ed1fc1b
sroycode <sroycode@gmail.com>
2025-05-28 13:40:47 +0530

    WIP Backup [ci skip]

--- scripts/53_copy_to_12.sh
+++
@@ -1,3 +0,0 @@
-scp dist/*.whl shreos12:/var/www/production/python3_empowerpoint_net/tirjapy
-scp dist/*.whl shreos12:/var/www/production/python3_inspirepoint_net/tirjapy
-scp dist/*.whl shreos12:/var/www/production/python3_tirja_com/tirjapy
---
+++ scripts/53_copy_to_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export SRCDIR=$(dirname $(cd ${0%/*} 2>>/dev/null ; echo `pwd`/${0##*/}))
+. ${SRCDIR}/00_script_config.sh
+
+# main
+twine upload dist/*
